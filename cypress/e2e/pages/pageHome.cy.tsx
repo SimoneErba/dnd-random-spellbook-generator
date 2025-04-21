88b3d68b03614b797442f607664725da8dbc9758
Simone Erba <simone.erba@oncode.it>
2025-04-21 19:07:05 +0200

    Updates

--- cypress/e2e/pages/pageHome.cy.tsx
+++
@@ -1,6 +0,0 @@
-describe("<Home />", () => {
-  it("renders", () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.visit("http://localhost:3000");
-  });
-});
--- next.config.ts
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: "/dnd-random-spell-generator", // 👈 Your GitHub repo name
-  assetPrefix: "/dnd-random-spell-generator",
-};
